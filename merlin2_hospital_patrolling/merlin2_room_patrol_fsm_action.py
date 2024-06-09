f9ab5ed8c69a084246645ed1c115b8b3db5e7f25
RogerFerEnr <148997601+RogerFerEnr@users.noreply.github.com>
2024-06-09 22:42:27 +0200

    cambios tarea merlin

--- merlin2_hospital_patrolling/merlin2_room_patrol_fsm_action.py
+++ merlin2_hospital_patrolling/merlin2_room_patrol_fsm_action.py
@@ -1,1 +1,107 @@
 # TODO: write the patrol FSM action
+import rclpy
+from typing import List
+from .pddl import room_type, room_at, room_patrolled
+from merlin2_basic_actions.merlin2_basic_types import wp_type
+from merlin2_basic_actions.merlin2_basic_predicates import robot_at
+from merlin2_fsm_action import Merlin2BasicStates
+from merlin2_fsm_action import Merlin2FsmAction
+
+from yasmin import Blackboard
+from yasmin_ros.basic_outcomes import SUCCEED
+from yasmin import CbState 
+from kant_dto import PddlObjectDto, PddlConditionEffectDto
+
+
+
+class Merlin2RoomPatrolFsmAction(Merlin2FsmAction):
+
+    def __init__(self) -> None:
+
+        self._room = PddlObjectDto(room_type, "room")
+        self._wp = PddlObjectDto(wp_type,"wp")
+
+        super().__init__("room_patrol")
+    
+        tts_state = self.create_state(Merlin2BasicStates.TTS)
+
+        self.add_state(
+            "PREPARING_TEXT",
+            CbState([SUCCEED],self.prepare_text),
+            transitions={
+                SUCCEED:"SPEAKING"
+            }
+        )
+        self.add_state(
+            "ROTATING",
+            CbState([SUCCEED],self.rotate),
+            transitions={
+                SUCCEED:"PREPARING_TEXT"
+            }
+        )
+#fALTA CREAR EL ESTADO DE GIRAR USANDO EL CMDVEL LE DECIMOS QUE GIRE DANDO UNA VUELTA COMPLETA
+#El estado de girar se puede hacer con un callback state
+        self.add_state(
+            "SPEAKING",
+            tts_state
+        )
+
+    def prepare_text(self, blackboard: Blackboard)-> str:
+
+        room_name = blackboard.merlin2_action_goal.objects[0][-1]      
+        blackboard.text = f"Stretcher room {room_name} patrolled"
+        return SUCCEED
+    
+    def rotate(self, blackboard: Blackboard)-> str:
+
+        #### Completar el código. Podemos usar un twist para que gire y otro con cero
+        return SUCCEED
+
+
+
+    def create_parameters(self) -> List[PddlObjectDto]:
+        return [self._room, self._wp]
+    
+    def create_conditions(self) -> List[PddlConditionEffectDto]:
+        
+        cond_1 = PddlConditionEffectDto(
+            room_patrolled,
+            [self._room],
+            PddlConditionEffectDto.AT_START,
+            is_negative = False
+        )#El robot tiene que estar en la habitación al principio de la acción
+        cond_2 = PddlConditionEffectDto(
+            robot_at,
+            [self._wp],
+            PddlConditionEffectDto.AT_START
+        ) #La habitación tiene que estar en el mismo lugar que el waypoint al principio de la acción
+        cond_3 = PddlConditionEffectDto(
+            room_at,
+            [self._room,self._wp],
+            PddlConditionEffectDto.AT_START    
+        )   
+
+        return [cond_1, cond_2, cond_3]     
+
+    def create_effects(self) -> List[PddlConditionEffectDto]:
+
+        effect_1 = PddlConditionEffectDto(
+            room_patrolled,
+            [self._room]
+            time=PddlConditionEffectDto.AT_END 
+        )
+        
+        return [effect_1]
+
+        return super().create_conditions()
+
+
+
+def main():
+    rclpy.init()
+    node = Merlin2RoomPatrolFsmAction()
+    node.join_spin()
+    rclpy.shutdown()
+
+if __name__ == "main":
+    main()
--- merlin2_hospital_patrolling/pddl.py
+++ merlin2_hospital_patrolling/pddl.py
@@ -1,1 +1,14 @@
 # TODO: write the PDDL domain
+
+
+from merlin2_basic_actions.merlin2_basic_types import wp_type
+from kant_dto import PddlTypeDTO, PddlPredicateDto
+
+room_type = PddlTypeDto("room")
+
+room_patrolled = PddlPredicateDto("room_patrolled",[room_type])
+
+#Hay que decir que waypoint corresponde a cada ubicación
+room_at = PddlPredicateDto("room_at",[room_type,wp_type])
+#Predicado para decir en que waypoint está cada habitacion
+#Con esto ya se ha definido el dominio
